19e2683878d70c0ac75796c1ef18c690704f7f5e
WagnerReis <wagnerreis1997@gmail.com>
2023-11-15 21:57:28 -0300

    feat: add user dto

--- src/http/dto/user/create-user.dto.ts
+++ src/http/dto/user/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsDate, IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @IsDate()
+  created_at?: Date;
+
+  @IsDate()
+  updated_at?: Date;
+}
