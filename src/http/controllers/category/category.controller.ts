d199421d1f0e1b6d1db8b4dddd44b189c79f7b73
WagnerReis <wagnerreis1997@gmail.com>
2023-11-18 17:21:49 -0300

    docs: add docs for categories

--- src/http/controllers/category/category.controller.ts
+++ src/http/controllers/category/category.controller.ts
@@ -12,26 +12,55 @@ import {
 import { CategoryService } from '../../../services/category/category.service';
 import { CreateCategoryDto } from '../../dto/category/create-category.dto';
 import { UpdateCategoryDto } from '../../dto/category/update-category.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('category')
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create category' })
+  @ApiResponse({
+    status: 201,
+    description: 'Category created successfully',
+  })
+  @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
+  @ApiOperation({ summary: 'Find categories' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of categories',
+  })
   @Get()
   findAll() {
     return this.categoryService.findAll();
   }
 
+  @ApiOperation({ summary: 'Find category' })
+  @ApiResponse({
+    status: 200,
+    description: 'Category data',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update category' })
+  @ApiResponse({
+    status: 200,
+    description: 'Category data',
+  })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -40,6 +69,11 @@ export class CategoryController {
     return this.categoryService.update(id, updateCategoryDto);
   }
 
+  @ApiOperation({ summary: 'Delete category' })
+  @ApiResponse({
+    status: 200,
+    description: 'Category data',
+  })
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   remove(@Param('id') id: string) {
